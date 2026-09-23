1b1d4fa295ff1d00da0ccb1c06942e39f396b1f7
abbbbb12138 <hanhamtakeru@gmail.com>
2026-09-23 19:36:09 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import streamlit as st
+
+st.title("今日心情小测试")
+
+mood_index = st.number_input(
+    "请输入您今天的心情：",
+    min_value=0.0,
+    max_value=100.0,
+    value=None,
+    placeholder="请输入0-100之间的数字"
+)
+
+at_home = st.selectbox("您今天是否在家：", ["yes", "no"])
+
+if mood_index is not None:
+    if mood_index >= 50 and at_home == "yes":
+        st.write("我来找你玩啦！")
+    elif mood_index < 50 and at_home == "yes":
+        st.write("我马上来安慰你！")
+    elif mood_index >= 50 and at_home == "no":
+        st.write("好吧，我下次再来找你")
+    else:
+        st.write("那好吧")
